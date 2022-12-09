e8210d006e27be574d0153940dbfa877d276342c
EasilyFire <tonghongchao@testbird.com>
2022-12-09 11:24:28 +0800

    1.0.0

--- app/src/main/java/com/demo/safelocker/app/SafeApp.kt
+++ app/src/main/java/com/demo/safelocker/app/SafeApp.kt
@@ -1,11 +1,14 @@
 package com.demo.safelocker.app
 
 import android.app.Application
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
 import com.tencent.mmkv.MMKV
 
 class SafeApp:Application() {
     override fun onCreate() {
         super.onCreate()
         MMKV.initialize(this)
+        Firebase.initialize(this)
     }
 }
--- app/src/main/java/com/demo/safelocker/config/SafeLocalConfig.kt
+++ app/src/main/java/com/demo/safelocker/config/SafeLocalConfig.kt
@@ -1,6 +1,6 @@
 package com.demo.safelocker.config
 
 object SafeLocalConfig {
-    const val email=""
-    const val url=""
+    const val email="yalingantetomaso@gmail.com"
+    const val url="https://sites.google.com/view/safe-lockerapp/%E9%A6%96%E9%A1%B5"
 }
